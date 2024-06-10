c746c226b2c5acb565158b7f26be8e3600353f63
Chan Qijian <neverhack12345@gmail.com>
2024-06-10 22:32:35 +0800

    Use Fragment for Home and Forms

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,20 +1,23 @@
+import { Fragment } from "react";
 import logo from "../logo.svg";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <Fragment>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </Fragment>
   );
 }
