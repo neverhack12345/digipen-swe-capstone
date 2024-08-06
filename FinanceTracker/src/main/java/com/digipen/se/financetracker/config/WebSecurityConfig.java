a62f7a65dbcc8a3ff1ad528facde930141deb196
Chan Qijian <34153069+neverhack12345@users.noreply.github.com>
2024-08-06 09:16:01 +0800

    Add Spring Security with basic authentication

---
+++ FinanceTracker/src/main/java/com/digipen/se/financetracker/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.digipen.se.financetracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) ->
+                auth.anyRequest().permitAll()).httpBasic(withDefaults())
+                .csrf(c->c.ignoringRequestMatchers("/**").disable());
+        return http.build();
+    }
+}
