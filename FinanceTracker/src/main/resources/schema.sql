e4f9d55c5a2a310bad224277b6dc43a792c42f03
Chan Qijian <34153069+neverhack12345@users.noreply.github.com>
2024-08-06 09:15:45 +0800

    Update database schema to include unique columns

--- FinanceTracker/src/main/resources/schema.sql
+++ FinanceTracker/src/main/resources/schema.sql
@@ -7,25 +7,25 @@ DROP TABLE IF EXISTS user_account;
 
 /* Create the schema for our tables */
 CREATE TABLE user_account (user_id INT AUTO_INCREMENT,
-    email VARCHAR(255), password VARCHAR (255), 
+    email VARCHAR(255) UNIQUE, password VARCHAR (60),
 	first_name VARCHAR(255), last_name VARCHAR(255), 
 	dob DATE, gender VARCHAR(1),
     PRIMARY KEY(user_id) 
 	) engine=InnoDB;
 
 CREATE TABLE category (cat_id INT AUTO_INCREMENT,
-    cat_name VARCHAR(255), 
-    PRIMARY KEY(cat_id) 
+    cat_name VARCHAR(255) UNIQUE,
+    PRIMARY KEY(cat_id)
 	) engine=InnoDB;
 
 CREATE TABLE sub_category (sub_id INT AUTO_INCREMENT,
-    cat_id INT, sub_name VARCHAR(255),
+    cat_id INT NOT NULL, sub_name VARCHAR(255) UNIQUE,
     PRIMARY KEY(sub_id),
 	FOREIGN KEY (cat_id) REFERENCES category(cat_id) ON DELETE CASCADE
 	) engine=InnoDB;	
 
 CREATE TABLE cash_flow (flow_id INT AUTO_INCREMENT,
-    user_id INT, sub_id INT,
+    user_id INT NOT NULL, sub_id INT NOT NULL,
 	source_name VARCHAR(255), date DATE,
 	amount DECIMAL(12,2), remark VARCHAR(255),
     PRIMARY KEY(flow_id),
@@ -34,7 +34,7 @@ CREATE TABLE cash_flow (flow_id INT AUTO_INCREMENT,
 	) engine=InnoDB;
 
 CREATE TABLE budget (budget_id INT AUTO_INCREMENT,
-    user_id INT, cat_id INT, 
+    user_id INT NOT NULL, cat_id INT NOT NULL,
 	year INT, month INT,
 	amount DECIMAL(12,2),
     PRIMARY KEY(budget_id),
