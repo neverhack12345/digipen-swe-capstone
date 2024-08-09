cefbe2149623ad8feea54e56a75a3b98da7a931e
Chan Qijian <neverhack12345@gmail.com>
2024-08-10 01:09:31 +0800

    Implement user registration

--- FinanceTrackerFront/my-app/app/api/route.tsx
+++ FinanceTrackerFront/my-app/app/api/route.tsx
@@ -3,10 +3,20 @@
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 import { redirect, permanentRedirect } from "next/navigation";
-import { AddBudget, EditBudget } from "@/types/definitions";
+import { AddBudget, AddUser, EditBudget } from "@/types/definitions";
 
 const LOGIN_TAG = "isLoggedin";
 const USER_ID = "userId";
+const LOGOUT_DELETE = [LOGIN_TAG, LOGIN_TAG]
+
+export async function sampleAPI(formData: FormData) {
+  // const response = await fetch('/api/submit', {
+  //     method: 'POST',
+  //     body: formData,
+  //   })
+  // revalidatePath("/signup")
+  // redirect("/login");
+}
 
 export async function fetchBudgets() {
   try {
@@ -30,31 +40,31 @@ export async function fetchBudgets() {
 }
 
 export async function addBudget(formData: AddBudget) {
-    try {
-        const response = await fetch('http://localhost:8080/api/budget/add', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            ...formData,
-            [USER_ID]: cookies().get(USER_ID),
-          },),
-        });
+  try {
+      const response = await fetch('http://localhost:8080/api/budget/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          [USER_ID]: cookies().get(USER_ID),
+        },),
+      });
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data.message);
-        } else {
-          const errorData = await response.json();
-          console.log(errorData.message || 'There was an error submitting the form.');
-        }
-    } catch (error) {
-        console.log('There was an error submitting the form.');
-        console.error('Form submission error:', error);
-    }
-    revalidatePath("/budget")
-    redirect("/budget")
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.message);
+      } else {
+        const errorData = await response.json();
+        console.log(errorData.message || 'There was an error submitting the form.');
+      }
+  } catch (error) {
+      console.log('There was an error submitting the form.');
+      console.error('Form submission error:', error);
+  }
+  revalidatePath("/budget")
+  redirect("/budget")
 }
 
 export async function deleteBudget(budgetId: any) {
@@ -106,34 +116,46 @@ export async function editBudget(formData: EditBudget) {
   redirect("/budget")
 }
 
-export async function sampleAPI(formData: FormData) {
-    // const response = await fetch('/api/submit', {
-    //     method: 'POST',
-    //     body: formData,
-    //   })
-    // revalidatePath("/signup")
-    // redirect("/login");
-}
+export async function createUser(formData: AddUser) {
+  try {
+    const response = await fetch('http://localhost:8080/api/user/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
 
-export async function createUser() {
-    revalidatePath("/signup")
-    redirect("/login");
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data.message);
+    } else {
+      const errorData = await response.json();
+      console.log(errorData.message || 'There was an error submitting the form.');
+    }
+  } catch (error) {
+      console.log('There was an error submitting the form.');
+      console.error('Form submission error:', error);
+  }
+  revalidatePath("/signup")
+  redirect("/login");
 }
 
 export async function authenticateUser() {
-    cookies().set(LOGIN_TAG, "true")
-    cookies().set(USER_ID, "1")
-    revalidatePath("/login")
-    redirect("/category");
+  cookies().set(LOGIN_TAG, "true")
+  cookies().set(USER_ID, "1")
+  revalidatePath("/login")
+  redirect("/category");
 }
 
 export async function logoutUser() {
-    cookies().delete(LOGIN_TAG);
-    cookies().delete(USER_ID);
-    revalidatePath("/")
-    redirect("/login");
+  LOGOUT_DELETE.forEach((item) => {
+    cookies().delete(item);
+  })
+  revalidatePath("/")
+  redirect("/login");
 }
 
 export async function isLoggedin() {
-    return cookies().has(LOGIN_TAG);
+  return cookies().has(LOGIN_TAG);
 }
--- FinanceTrackerFront/my-app/app/signup/signup-form.tsx
+++ FinanceTrackerFront/my-app/app/signup/signup-form.tsx
@@ -8,19 +8,74 @@ import { Divider } from "@nextui-org/divider";
 import { Select, SelectItem } from "@nextui-org/select";
 import { DatePicker } from "@nextui-org/date-picker";
 import { createUser } from "../api/route";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { z } from "zod";
+import { AddUser, FormErrors } from "@/types/definitions";
 
 export default function SignUp() {
-    async function onSubmit(event: FormEvent<HTMLFormElement>) {
-      event.preventDefault()
-      await createUser();
-    }
+  const [errors, setErrors] = useState<FormErrors<AddUser>>({});
+  const [formData, setFormData] = useState<AddUser>({
+    "email": "",
+    "password": "",
+    "firstName": "",
+    "lastName": "",
+    "dob": "",
+    "gender": "",
+  });
+  
+  
+  const AddBudgetSchema = z.object({
+    userId: z.coerce.number().int().min(1, "User id cannot be less than 1"),
+    categoryId: z.coerce.number().int().min(1, "Category id cannot be less than 1"),
+    year: z.coerce.number().int().min(1800, "Year cannot be before 1800"),
+    month: z.coerce.number().int().min(1, "Month cannot be less than 1").max(12, "Month cannot be more than 12"),
+    amount: z.coerce.number()
+  });  
 
-    const gender = [
-      {key: "unkown", label: "N/A"},
-      {key: "female", label: "F"},
-      {key: "male", label: "M"},
-    ];
+  const validateForm = (data: AddUser): FormErrors<AddUser> => {
+    try {
+      AddBudgetSchema.parse(data);
+      return {};
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return error.flatten().fieldErrors;
+      }
+      return {};
+    }
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newErrors = validateForm(formData);
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length === 0 && formData !== undefined) {
+      createUser(formData);
+    }
+  }
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    if (e?.target?.name != undefined && e?.target?.value != undefined) {
+      if (formData !== undefined) {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value,
+        });
+      };
+    }
+  }
+  const handleSelectionChange = (key: string, value: string) => {
+    setFormData({
+      ...formData,
+      [key]: value,
+    });
+  }
+
+  const gender = [
+    {key: "unkown", label: "N/A"},
+    {key: "female", label: "F"},
+    {key: "male", label: "M"},
+  ];
 
     return (
       <Card className="border-solid border-indigo-500 bg-background/60 dark:bg-default-100/50 max-w-[610px]" 
@@ -37,16 +92,24 @@ export default function SignUp() {
           <p className="text-primary">Sign-up</p>
           <div className="w-full flex-wrap gap-4">
             <Input type="Email" label="Email" placeholder="Enter your email" 
+            name="email" value={formData.email} onChange={handleChange}
             color='default' variant='bordered' size='md'  radius='full'/>
             <Input type="Password" label="Password" placeholder="Enter your password" 
+            name="password" value={formData.password} onChange={handleChange}
             color='default' variant='bordered' size='md' radius='full'/>
             <Input type="Text" label="First Name" placeholder="Enter your first name" 
+            name="firstName" value={formData.firstName} onChange={handleChange}
             color='default' variant='bordered' size='md' radius='full'/>
             <Input type="Text" label="Last Name" placeholder="Enter your last name" 
+            name="lastName" value={formData.lastName} onChange={handleChange}
             color='default' variant='bordered' size='md' radius='full'/>
             <DatePicker
               label="Birthday"
               maxValue={today(getLocalTimeZone())}
+              onChange={(e) => (setFormData({
+                ...formData,
+                "dob": e.toString(),
+              }))}
               color='default' variant='bordered' size='md' radius='full'
             />
             <Select
@@ -55,6 +118,7 @@ export default function SignUp() {
               placeholder="Select an gender"
               className="max-w-xs"
               color='default' variant='bordered' size='md' radius='full'
+              onChange={(e) => handleSelectionChange("gender", e.target.value)}
             >
               {(gender) => <SelectItem key={gender.key}>{gender.label}</SelectItem>}
             </Select>
