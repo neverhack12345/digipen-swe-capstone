eba19d1c4867f4cf2314a80746d4b8955d3600e0
Chan Qijian <neverhack12345@gmail.com>
2024-08-14 00:54:48 +0800

    Added simple donut chart to budget page

--- FinanceTrackerFront/my-app/app/budget/page.tsx
+++ FinanceTrackerFront/my-app/app/budget/page.tsx
@@ -6,6 +6,38 @@ import { useCallback, useEffect, useState } from "react";
 import BudgetTable from "./budget-table";
 import { Budget } from "@/types/definitions";
 import { fetchBudgets } from "../api/route";
+import { Pie, PieChart } from "recharts"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartConfig = {
+  amount: {
+    label: "Amount",
+  },
+  "Auto & Transport": {
+    label: "Auto & Transport",
+    // color: "hsl(var(--chart-1))",
+  },
+  "Bills & Utilities": {
+    label: "Bills & Utilities",
+    // color: "hsl(var(--chart-2))",
+  },
+  "Business": {
+    label: "Business",
+    // color: "hsl(var(--chart-3))",
+  },
+  "Children": {
+    label: "Children",
+    // color: "hsl(var(--chart-4))",
+  },
+  "Education": {
+    label: "Education",
+    // color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig
 
 export default function BudgetPage() {
   const [year, setYear] = useState<string>((new Date().getFullYear()).toString())
@@ -13,6 +45,13 @@ export default function BudgetPage() {
   const [data , setData] = useState<Array<Budget>>([]);
   const [filteredData, setFilteredData] = useState<Array<Budget>>([]);
   const [isCompleteLoaded, setIsCompleteLoaded] = useState(false);
+  const [chartData, setChartData] = useState([
+    { category: "Auto & Transport", amount: 275, fill: "hsl(var(--chart-1))" },
+    { category: "Bills & Utilities", amount: 200, fill: "hsl(var(--chart-2))" },
+    { category: "Business", amount: 187, fill: "hsl(var(--chart-3))" },
+    { category: "Children", amount: 173, fill: "hsl(var(--chart-4))" },
+    { category: "Education", amount: 90, fill: "hsl(var(--chart-5))" },
+  ])
 
   const fetchData = useCallback(async () => {
     const budgets = await fetchBudgets();
@@ -41,6 +80,11 @@ export default function BudgetPage() {
 
       return cmp;
     });
+    let counter = 1;
+    const result = filteredBudgets.map((budget) => {
+      return {category: budget.catName, amount: budget.amount, fill: `hsl(var(--chart-${counter++}))`}
+    })
+    setChartData(result)
     
     setFilteredData(filteredBudgets);
   }, [month, year, data]);
@@ -67,6 +111,25 @@ export default function BudgetPage() {
       {(month) => <SelectItem key={month.key} value={month.key}>{month.label}</SelectItem>}
       </Select>
       </div>
+      <Skeleton isLoaded={isCompleteLoaded} className="rounded-lg">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="amount"
+              nameKey="category"
+              innerRadius={60}
+            />
+          </PieChart>
+        </ChartContainer>
+      </Skeleton>
       <Skeleton isLoaded={isCompleteLoaded} className="rounded-lg">
         <BudgetTable filteredData={filteredData} fetchData={fetchData} />
       </Skeleton>
